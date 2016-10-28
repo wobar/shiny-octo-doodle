eed1c0321e79544d0754bb55a605582346a6a041
Wojciech_Bartus <wojciech_bartus@epam.com>
2016-10-28 17:01:05 +0200

    HTML5 features testing project - initial

---
+++ HTML5 features/custom_scripts.js
@@ -0,0 +1,9 @@
+
+function loadGeolocation() {
+    document.getElementById('presentationContainer').innerHTML="'<object type='text/html' data='Geolocation_subpage.html' ></object>";
+}
+
+function loadDragAndDrop() {
+    document.getElementById('presentationContainer').innerHTML="'<object type='text/html' data='DragAndDrop_subpage.html' ></object>";
+}
+
